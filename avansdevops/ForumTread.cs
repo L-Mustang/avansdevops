67806ff6bd3a9321fcf217d330a1086da471ef40
Laerositus <jelleboy22@gmail.com>
2022-04-13 10:33:23 +0200

    Implemented Forum

---
+++ avansdevops/ForumTread.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avansdevops
+{
+    public class ForumTread
+    {
+        private List<ForumPost> _posts;
+        private BacklogItem _backlogItem
+        private string _title;
+
+        public ForumThread(string title, BacklogItem backlogItem)
+        {
+            _title = title;
+            _backlogItem = backlogItem;
+
+            _posts = new List<ForumPost>();
+        }
+
+        public void AddPost(ForumPost post)
+        {
+            if (post != null)
+            {
+                _posts.Add(post);
+            }
+        }
+    }
+}
