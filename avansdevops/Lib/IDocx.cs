d33d4c1883cd3bc49a8adce46f39c4e78030f1fe
LordMustang <elionass58@gmail.com>
2022-04-19 19:34:50 +0200

    Added initial implementation of mock APIs

---
+++ avansdevops/Lib/IDocx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avansdevops.Lib
+{
+    public interface IDocx
+    {
+        string Text { get; set; }
+    }
+}
---
+++ avansdevops/Lib/WordAPI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avansdevops.Lib
+{
+    public class WordAPI
+    {
+        private string _extension;
+        
+        public WordAPI(string extension)
+        {
+            _extension = extension;
+        }
+
+        public string Extension
+        {
+            get { return _extension; }
+            set { _extension = value; }
+        }
+
+        public void SaveDocx(string fileName, string text)
+        {
+            Console.Out.WriteLine("Saving to file: {0}.{1}", fileName, _extension);
+            Console.Out.WriteLine(text);
+        }
+    }
+}
