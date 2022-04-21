03de3a3cf32cf14b30035651d411884de3accbd6
LordMustang <elionass58@gmail.com>
2022-04-21 23:28:09 +0200

    Implemented composition pattern tests for User

--- avansdevopsTests/SprintTests.cs
+++ avansdevopsTests/SprintTests.cs
@@ -12,11 +12,17 @@ namespace avansdevopsTests
         Sprint? sprintFeedback;
         Sprint? sprintRelease;
 
+        UserFactory? factory;
+        IUser? user;
+
         [TestInitialize()]
         public void Startup()
         {
             sprintFeedback = new Sprint(new SprintStrategyFeedback());
             sprintRelease = new Sprint(new SprintStrategyRelease());
+
+            factory = new DeveloperFactory("Jan", "Jantjes", "mail@mail.com");
+            user = factory.CreateUser();
         }
 
         [TestMethod]
@@ -37,13 +43,54 @@ namespace avansdevopsTests
             sprintRelease.GetStrategy().Should().BeOfType<SprintStrategyRelease>();
         }
 
+        public void Test_SprintGetStatus()
+        {
+            // Arrange
+            // Act
+            // Assert
+            sprintFeedback.GetStatus().Should().BeTrue();
+        }
+
         [TestMethod]
         public void Test_SprintSetStatus()
         {
             // Arrange
             // Act
-            sprintFeedback.SetStatus(true);
+            sprintFeedback.SetStatus(false);
             // Assert
+            sprintFeedback.GetStatus().Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Test_SprintAddUser_GetUser()
+        {
+            // Arrange
+            // Act
+            sprintFeedback.AddUser(user);
+            // Assert
+            sprintFeedback.GetUser(user.FullName).Should().Be(user);
+        }
+
+        [TestMethod]
+        public void Test_SprintAddUser_GetAllUsers()
+        {
+            // Arrange
+            // Act
+            sprintFeedback.AddUser(user);
+            sprintFeedback.AddUser(user);
+            // Assert
+            sprintFeedback.GetAllUsers().Count.Should().Be(2);
+        }
+
+        [TestMethod]
+        public void Test_SprintRemoveUser()
+        {
+            // Arrange
+            // Act
+            sprintFeedback.AddUser(user);
+            sprintFeedback.RemoveUser(user);
+            // Assert
+            sprintFeedback.GetAllUsers().Count.Should().Be(0);
         }
     }
 }
