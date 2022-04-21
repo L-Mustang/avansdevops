5540b3423209f5426f27e37fb98207f293c16bd2
LordMustang <elionass58@gmail.com>
2022-04-21 15:31:34 +0200

    Added DevOps

---
+++ avansdevops/DevOps/DevOps.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avansdevops.DevOps
+{
+    public class DevOps
+    {
+        public DevOps()
+        {
+
+        }
+
+        public void InstallPackage(string packageUrl)
+        {
+            Console.WriteLine("Downloading package from: {0}", packageUrl);
+        }
+
+        public void Deploy()
+        {
+            Console.WriteLine("Deployed");
+        }
+
+        public void Test()
+        {
+            Console.WriteLine("Testing..");
+            Console.WriteLine("Finished all tests");
+        }
+
+        public void Build()
+        {
+            Console.WriteLine("Building...");
+            Console.WriteLine("Build completed");
+
+        }
+
+        public void Analyse()
+        {
+            Console.WriteLine("Analysing...");
+            Console.WriteLine("Analysis completed. ")
+        }
+    }
+
+}
