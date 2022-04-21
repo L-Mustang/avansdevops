215baf5043d738bddf2f00ac3262205394c8b998
Laerositus <33722419+Laerositus@users.noreply.github.com>
2022-04-21 16:37:54 +0200

    Implemented Devops TestingAdapters

---
+++ avansdevops/Lib/NUnit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using avansdevops.DevOps;
+
+namespace avansdevops.Lib
+{
+    public class NUnit
+    {
+        public void TestNUnit(Branch branch)
+        {
+            Console.WriteLine($"Testing with NUnit");
+        }
+
+
+    }
+}
